c70e29d3c06da981ed0005f351db1cc9424a5994
litfinger <505565456@qq.com>
2020-12-11 15:38:56 +0800

    小改，不影响

--- src/router/index.js
+++ src/router/index.js
@@ -172,4 +172,9 @@ router.beforeEach((to, from, next) => {
   next();
 })
 
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 export default router;
